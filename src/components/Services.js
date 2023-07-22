db13ef1a1cdb6924da2504cd5a8def6ebf33f7fa
CGrace <c.grace5@yahoo.com.au>
2023-07-22 15:55:53 +1000

    Baseline of the site done. Need to replace images and add remaining links

---
+++ src/components/Services.js
@@ -0,0 +1,104 @@
+import React from 'react';
+// icon
+import { BsArrowUpRight } from 'react-icons/bs';
+// motion
+import { motion } from 'framer-motion';
+// variants
+import { fadeIn } from '../variants';
+
+// services data
+const services = [
+  {
+    name: 'UI/UX Design',
+    description:
+      'Whether solo of with the customers guidance and input, the website I create will look, feel and function exactly as requested.',
+    link: 'Learn more',
+  },
+  {
+    name: 'Development',
+    description:
+      'From the ground up I can and will build the perfect website to suit your personal or business requirements.',
+    link: 'Learn more',
+  },
+  {
+    name: 'Digital Marketing',
+    description:
+      'Let me help your website be noticed and stand apart from its billions of rivals.',
+    link: 'Learn more',
+  },
+  {
+    name: 'Product maintenance',
+    description:
+      'Extending beyond the construction of the website, I have maintenace and update services availiable to ensure that any change or modification is only an email away.',
+    link: 'Learn more',
+  },
+];
+
+const Services = () => {
+  return (
+    <section className='section' id='services'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col lg:flex-row'>
+          {/* text & image */}
+          <motion.div
+            variants={fadeIn('right', 0.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0'
+          >
+            <h2 className='h2 text-accent mb-6'>What I Do.</h2>
+            <h3 className='h3 max-w-[455px] mb-16'>
+              I'm a Developer beginning my journey into the world of web development, where the magic of realising clients dreams is my expectation.
+            </h3>
+            <button className='btn btn-sm'>See my work</button>
+          </motion.div>
+          {/* services */}
+          <motion.div
+            variants={fadeIn('left', 0.5)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1'
+          >
+            {/* service list */}
+            <div>
+              {services.map((service, index) => {
+                // destructure service
+                const { name, description, link } = service;
+                return (
+                  <div
+                    className='border-b border-white/20 h-[146px] mb-[38px] flex'
+                    key={index}
+                  >
+                    <div className='max-w-[476px]'>
+                      <h4 className='text-[20px] tracking-wider font-primary font-semibold mb-6'>
+                        {name}
+                      </h4>
+                      <p className='font-secondary leading-tight'>
+                        {description}
+                      </p>
+                    </div>
+                    <div className='flex flex-col flex-1 items-end'>
+                      <a
+                        href='#'
+                        className='btn w-9 h-9 mb-[42px] flex justify-center items-center'
+                      >
+                        <BsArrowUpRight />
+                      </a>
+                      <a href='#' className='text-gradient text-sm'>
+                        {link}
+                      </a>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
---
+++ src/components/Work.js
@@ -0,0 +1,111 @@
+import React from 'react';
+// motion
+import { motion } from 'framer-motion';
+// variants
+import { fadeIn } from '../variants';
+// img
+import Img1 from '../assets/CClogoWhite.png';
+import Img2 from '../assets/Movie buffs logo.png';
+import Img3 from '../assets/webfactory-ltd-NoOrDKxUfzo-unsplash.jpg';
+
+const Work = () => {
+  return (
+    <section className='section' id='work'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col lg:flex-row gap-x-10'>
+          <motion.div
+            variants={fadeIn('right', 0.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'
+          >
+            {/* text */}
+            <div>
+              <h2 className='h2 leading-tight text-accent'>
+                My Latest <br />
+                Work.
+              </h2>
+              <p className='max-w-sm mb-16'>
+                Here is a collection of my most substantial projects.
+
+                The movie buffs - a front end website for cinephiles who wish to view what is now showing, coming soon and to purchase tickets.
+
+                Choice Cuts - a full stack digital shopfront for a fictional butchery.
+                Coming soon - a full stack website utilising the most modern technologies.
+              </p>
+              <button className='btn btn-sm'>View all projects </button>
+            </div>
+            {/* image */}
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+              {/* overlay */}
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+              {/* img */}
+              <img
+                className='group-hover:scale-125 transition-all duration-500'
+                src={Img1}
+                alt=''
+              />
+              {/* pretitle */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className='text-gradient'>Choice Cuts</span>
+              </div>
+              {/* title */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                <span className='text-3xl text-white'>A full stack digital shopfront.</span>
+              </div>
+            </div>
+          </motion.div>
+          <motion.div
+            variants={fadeIn('left', 0.2)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1 flex flex-col gap-y-10'
+          >
+            {/* image */}
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+              {/* overlay */}
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+              {/* img */}
+              <img
+                className='group-hover:scale-125 transition-all duration-500'
+                src={Img2}
+                alt=''
+              />
+              {/* pretitle */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className='text-gradient'>Movie Buffs</span>
+              </div>
+              {/* title */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                <span className='text-3xl text-white'>Front end cinema website</span>
+              </div>
+            </div>
+            {/* image */}
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+              {/* overlay */}
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+              {/* img */}
+              <img
+                className='group-hover:scale-125 transition-all duration-500'
+                src={Img3}
+                alt=''
+              />
+              {/* pretitle */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className='text-gradient'>Full stack project</span>
+              </div>
+              {/* title */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                <span className='text-3xl text-white'>Coming soon!</span>
+              </div>
+            </div>
+          </motion.div>
+        </div>
+      </div >
+    </section >
+  );
+};
+
+export default Work;
